c40fb27324d52ccbba4cdbd5029e720f6d4ffc0f
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-11-18 17:58:55 -0500

    working on styling and devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013213631) do
+ActiveRecord::Schema.define(version: 20141118224629) do
 
   create_table "reports", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141013213631) do
     t.string   "cover_image_content_type"
     t.integer  "cover_image_file_size"
     t.datetime "cover_image_updated_at"
+    t.string   "description"
   end
 
   create_table "users", force: true do |t|
